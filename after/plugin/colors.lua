a5f5ece2fe8e3340a7de181ddfc36578e335d44e
Longoria Yu <longoria.yu@thecarousell.com>
2023-04-03 15:18:29 +0800

    Update background color to transparent

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,7 +3,7 @@ require("rose-pine").setup({
 	dark_variant = 'main',
 	bold_vert_split = false,
 	dim_nc_background = false,
-	disable_background = false,
+	disable_background = true,
     disable_float_background = false,
 	disable_italics = false,
 
@@ -50,8 +50,8 @@ function ColorMyPencils(color)
 	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
 
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
