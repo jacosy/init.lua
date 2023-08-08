012edff72b18918c0b7a6ba0893b53fcd0028b51
Longoria Yu <longoria.yu@thecarousell.com>
2023-08-08 23:52:47 +0800

    Add a new map for closing the quickfix window

--- lua/longoria/remap.lua
+++ lua/longoria/remap.lua
@@ -25,8 +25,8 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
+-- close quickfix window
+vim.keymap.set("n", "<C-c>", ":cclose<CR>", { noremap = true })
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
