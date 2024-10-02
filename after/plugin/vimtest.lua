4fd8ecbd071658f163a9e4cf732cc5bd5ed83c74
Longoria Yu <longoria.yu@thecarousell.com>
2024-10-02 12:14:29 +0800

    Add vim-test lua config

--- after/plugin/vimtest.lua
+++ after/plugin/vimtest.lua
@@ -1,6 +1,6 @@
 vim.keymap.set('n', '<leader>t', ':TestNearest<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>T', ':TestFile<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>a', ':TestSuite<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>s', ':TestSuite<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>l', ':TestLast<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>g', ':TestVisit<CR>', { noremap = true, silent = true })
 vim.cmd('let test#strategy = "vimux"')
