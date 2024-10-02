179e9e346552295e3d198ce9d647bb87b9f90f64
Longoria Yu <longoria.yu@thecarousell.com>
2024-10-02 13:37:41 +0800

    Remove vim-test and update the telescope tag

--- lua/longoria/packer.lua
+++ lua/longoria/packer.lua
@@ -8,7 +8,7 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
         -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
@@ -74,7 +74,4 @@ return require('packer').startup(function(use)
     use("fatih/vim-go", { run = ':GoUpdateBinaries' })
     use("akinsho/bufferline.nvim", { tag = '*', requires = 'nvim-tree/nvim-web-devicons' })
     use("christoomey/vim-tmux-navigator")
-    use {
-        "vim-test/vim-test", requires = { { "preservim/vimux" } }
-    }
 end)
