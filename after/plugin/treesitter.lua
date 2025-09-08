e01280d43bc78445a95c6d3c7ef459025b239e32
jacosy <longoria.you@gmail.com>
2025-09-08 15:46:13 +0800

    Add rust & bash parsers

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
     ensure_installed = { "go", "gomod", "gosum", "gowork", "javascript", "typescript", "python", "lua", "vim",
-        "query", "proto", "json", "markdown", "markdown_inline", "yaml", },
+        "query", "proto", "json", "markdown", "markdown_inline", "yaml", "rust", "bash", },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
